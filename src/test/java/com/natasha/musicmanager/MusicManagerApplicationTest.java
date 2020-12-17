9760fce7767a75c23fd95952c26a7a9ed87e11dc
natashashafqat <natashashafqat@gmail.com>
2020-12-17 19:24:09 +0000

    feat: first commit

---
+++ src/test/java/com/natasha/musicmanager/MusicManagerApplicationTest.java
@@ -0,0 +1,12 @@
+package com.natasha.musicmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MusicManagerApplicationTest {
+    @Test
+    void contextLoads() {
+
+    }
+}
