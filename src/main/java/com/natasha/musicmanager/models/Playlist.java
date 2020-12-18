c5998548a520e17c844ea8b0c28684d2d987e69f
natashashafqat <natashashafqat@gmail.com>
2020-12-18 10:42:28 +0000

    feat: update Playlist and Song models;

--- src/main/java/com/natasha/musicmanager/models/Playlist.java
+++ src/main/java/com/natasha/musicmanager/models/Playlist.java
@@ -1,4 +1,24 @@
 package com.natasha.musicmanager.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.math.BigInteger;
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "playlist")
 public class Playlist {
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    private BigInteger id;
+
+    private String name;
+
+    @Column(name = "created_on")
+    @JsonProperty("created_on")
+    private Date createdOn;
 }
